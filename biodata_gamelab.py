27f7379757fe64e5933354fc245a682c64027db0
PutraAdnyanaaa <127805378+PutraAdnyanaaa@users.noreply.github.com>
2023-08-20 07:59:58 +0800

    tambah file biodata

---
+++ biodata_gamelab.py
@@ -0,0 +1,23 @@
+print ("================================")
+
+print ("Program Biodata Sederhana Dengan Python")
+
+print ("================================")
+
+nama= "putra"
+
+hobi = "olahraga"
+
+alamat = "tabanan, Tabanan"
+
+Kota = "Tabanan"
+
+print("Nama Lengkap:",nama)
+
+print("Alamat",alamat, "\n           Kota ", Kota)
+
+print("Hobi:",hobi)
+
+
+
+print ("================================")
